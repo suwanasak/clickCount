708943e2ccac7b93cad8598eded16199a3c2f868
hlew <54799250+suwanasak@users.noreply.github.com>
2022-01-19 10:19:28 +0700

    Update qc_check_fix.php

--- qc_check_fix.php
+++ qc_check_fix.php
@@ -18,22 +18,21 @@ if (isset($_POST)){
 		$strSQL = "SELECT fix_code, sum(`fix_qty`) as count_fix, `fix_name`, `fix_date` 
 		FROM `count_fix` 
 		WHERE fix_name='$user_' AND fix_date between '$start_date' AND '$end_date' ";
-		$check_1 = $conn->query($strSQL)or die ("ERROR".$strSQL.'<br>'. print_r($conn->errorInfo(),true));
-		$row1 = $check_1->fetch();
+		$check_1 = mysqli_query($conn, $strSQL);
+		$row1 = mysqli_fetch_assoc($check_1);
 		$check_fix = isset($row1["count_fix"]) ? $row1["count_fix"] : ''; 
 		$check_code = $row1["fix_code"];
 		
-		$sum_check = "SSELECT `style_code`, sum(`style_amount`) AS qty  FROM `style_order` WHERE  style_code ='$check_code' ";
-		$check_2 = $conn->query($sum_check)or die ("ERROR".$sum_check.'<br>'. print_r($conn->errorInfo(),true));
-		$row2 = $check_2->fetch();
+		$sum_check = "SELECT `style_code`, sum(`style_amount`) AS qty  FROM `style_order` WHERE  style_code ='$check_code' ";
+		$check_2 = mysqli_query($conn, $sum_check);
+		$row2 = mysqli_fetch_assoc($check_2);
 		$check_amount = isset($row2["qty"]) ? $row2["qty"] : ''; 
 		
 		$check_balance = ($check_amount - $check_fix);
 		
-		$output[] = array("fix" => $check_fix,"amount" => $check_amount,"qty" => $check_balance);	
+		$output[] = array("fix" => $check_fix,"amount" => $check_amount,"qty" => $check_balance, 'ertete' => $check_code);	
 		
 		echo json_encode($output);	
 	}
 }
 ?>
-
